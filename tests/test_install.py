cd938da8e6d1139ca68dd31fa393e885cc222a2b
remiconnesson <remiconnesson2@gmail.com>
2019-03-01 11:15:49 +0100

    Create test_install.py

---
+++ tests/test_install.py
@@ -0,0 +1,6 @@
+# This must be a test that verifies thoroughly if installation of the environement is successful.
+
+TEST_PASSED = True
+
+if TEST_PASSED: print("Test Successful")
+else: print('Test Unsuccessful")
